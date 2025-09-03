a5d6d34ff9d94705845796dd615e42148066540f
Philippe Kurz <philippe.kurz@gmail.com>
2025-09-03 12:08:53 +0200

    Ajout exercice 1

---
+++ Exercice1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Exercice 1");
+
+// Enoncé
+// Créer une variable de type string et l'initialiser avec une chaîne de caractères (votre nom)
+// Afficher la variable dans la console
+string nom = "Dupont";
+Console.WriteLine(nom);
