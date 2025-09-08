e92f4dc82fe25a149d0330f9ce374c1db6dbb895
Philippe Kurz <philippe.kurz@gmail.com>
2025-09-08 15:14:37 +0200

    Fonctions

---
+++ boucles/Correction/Program.cs
@@ -0,0 +1,23 @@
+﻿﻿// Exercice 3
+
+// Ecrire un programme qui demande à l'utilisateur d'entrer des nombres (un par un).
+// La saisie s'arrête lorsqu'il entre 0.
+// Avec une boucle while, calculer et afficher la somme des nombres saisis.
+
+Console.WriteLine("Veuillez entrer un nombre (0 pour arrêter) :");
+
+int iterations = 1;
+int somme = 0;
+
+int nombre = Convert.ToInt32(Console.ReadLine());
+while (nombre != 0)
+{
+    somme += nombre;
+
+    nombre = Convert.ToInt32(Console.ReadLine());
+
+    iterations ++;
+}
+
+Console.WriteLine("Nombres d'itérations : " + iterations + ", somme = " + somme);
+Console.WriteLine("Boucle 3 terminée.");
---
+++ fonctions/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Fonctions");
+// Modèle abstrait d'une fonction
+// typeDeRetour NomDeLaFonction (Paremètres)
+// {
+//     Instructions...
+//     return valeurDeRetour
+// }
+
+
+int Additionne(int nbEntier1, int nbEntier2) // Déclaration de la fonction Additionne
+{
+    return nbEntier1 + nbEntier2;
+}
+
+int total = Additionne(2, 3);   //Appel de la fonction Additionne
+Console.WriteLine(total);
